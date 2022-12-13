0df754f6e9ccda32fb16fdf7a632b60dd62f51a7
Nursima Taşdemir <96255415+nursimatasdemir@users.noreply.github.com>
2022-12-13 17:43:14 +0300

    solana project

---
+++ pages/_app.tsx
@@ -0,0 +1,26 @@
+import type { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
+
+import { extendTheme } from "@chakra-ui/react"
+import WalletContextProvider from '../components/WalletContextProvider'
+
+const colors = {
+  background: "#1d144f",
+  accent: "#922ea3",
+  bodyText: "rgba(255, 255, 255, 0.75)",
+}
+
+const theme = extendTheme({ colors })
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <WalletContextProvider>
+      <Component {...pageProps} />
+      </WalletContextProvider>
+        
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
